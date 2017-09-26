24f4ea1c39c99c008852e8c71c808fcd987dbd94
rftpool13 <rftpool13@rz.fh-trier.de>
2017-09-26 13:19:42 +0200

    Uebung1

--- KaeuferTabelle.sql
+++ KaeuferTabelle.sql
@@ -1,3 +1,4 @@
+--Aufgabe 1.1
 CREATE TABLE kaeufer 
 (
 	Kundennr NUMBER(8) NOT NULL
@@ -6,3 +7,12 @@ CREATE TABLE kaeufer
 	, Erstkaufdatum DATE
 	, Kundenwert NUMBER(20,3)
 );
+
+--Aufgabe 1.2
+ALTER TABLE kaeufer ADD (anz_kinder NUMBER(2.0));
+--Aufgabe 1.3
+ALTER TABLE kaeufer DROP (Kundenwert);
+--Aufgabe 1.4
+ALTER TABLE kaeufer MODIFY (Vorname VARCHAR2(100));
+--Aufgabe 1.5
+ALTER TABLE kaeufer RENAME TO Kunde;
